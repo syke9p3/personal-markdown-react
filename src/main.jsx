7d4ca7dee793f47f5d5c8557a3d877e27dc14b58
Kenth Saya-ang <gargarkenth@gmail.com>
2024-05-07 14:38:11 +0800

    src changes

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,41 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
-import './index.css'
+import React, { createContext } from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App.jsx";
+import "./index.css";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import Login from "./pages/Login.jsx";
+import NotFound from "./pages/NotFound.jsx";
+import NotePage from "./pages/NotePage.jsx";
 
-ReactDOM.createRoot(document.getElementById('root')).render(
+let user = null;
+
+user = {
+  username: "syke9p3",
+  id: 1,
+};
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <App />,
+    // errorElement: <NotFound />,
+  },
+  {
+    path: "/login",
+    element: <Login />,
+  },
+  {
+    path: "/notes/:noteId",
+    element: <NotePage />,
+  },
+]);
+
+export const AuthContext = createContext({}); // paramter: initial value
+
+ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-)
+    <AuthContext.Provider value={user}>
+      <RouterProvider router={router} />
+    </AuthContext.Provider>
+  </React.StrictMode>
+);
